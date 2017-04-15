7afaadf45e6922ce494150a24a421db2e379cae6
namansancheti <naman.sancheti@gmail.com>
2017-04-15 12:16:01 +0530

    Added code to perform QuickSort in C++

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int N;
+
 int partition(int arr[], int low, int high)
 {
-	int pivot = arr[high];
-	
+	int pivot = arr[high]; // Pick the last element as the pivot
 	int i = low - 1;
 	
 	for(int j = low; j <= high - 1; j++)
@@ -17,11 +18,11 @@ int partition(int arr[], int low, int high)
 			arr[j] = temp;
 		}
 	}
-	int temp = arr[i + 1];
-	arr[i + 1] = arr[high];
-	arr[high] = temp;
 	
-	return i + 1;
+    int temp2 = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp2;
+	return i + 1; // Return the index of the pivot element
 }
 
 void quick_sort(int arr[], int low, int high)
@@ -29,25 +30,20 @@ void quick_sort(int arr[], int low, int high)
 	if(low < high)
 	{
 		int pi = partition(arr, low, high);
-		
 		quick_sort(arr, low, pi - 1);
 		quick_sort(arr, pi + 1, high);
 	}
+	
 }
 
 int main()
 {
-	int arr[] = {4, 2, 1, 10, 15};
-	int N = sizeof(arr) / sizeof(arr[0]);
+	int arr[] = {10, 7, 8, 9, 1, 5};
+	N = sizeof(arr) / sizeof(arr[0]);
 	
-	for(int i = 0; i < N; i++)
-	{
-		cout<<arr[i]<<" ";
-	}
-	cout<<"\n";
+	quick_sort(arr, 0, N - 1);
 	
-	quick_sort(arr, 0, N-1);
-	
+	cout<<"The sorted array is ";
 	for(int i = 0; i < N; i++)
 	{
 		cout<<arr[i]<<" ";
